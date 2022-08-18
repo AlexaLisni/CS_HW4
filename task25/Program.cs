cfa9dc21f6895af7e0746918cc36f9d090f2a00e
Alexandr <sashylay0@gmail.com>
2022-08-18 15:59:21 +0300

    finish task25

--- task25/Program.cs
+++ task25/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B)
+// и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.WriteLine("Ведите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("В какую степень его возвести? ");
+int power = Convert.ToInt32(Console.ReadLine());
+
+int PowerM(int number, int pow)
+{
+    int numberPow = number;
+    for(int i = 1; i < pow; i++)
+    {
+        number = number * numberPow;
+    }
+    return number;
+}
+
+int res = PowerM(num, power);
+Console.WriteLine($"число {num} в степени {power} = {res}");
