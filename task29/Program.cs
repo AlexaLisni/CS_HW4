c07e29e483f992197d8346d7992cd5fac5277b15
Alexandr <sashylay0@gmail.com>
2022-08-18 17:54:55 +0300

    add task30

--- task29/Program.cs
+++ task29/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
+Console.WriteLine("Ведите минимально число масива: ");
+int minNum = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Ведите максемальое число масива: ");
+int maxNum = Convert.ToInt32(Console.ReadLine());
+int[] res = FillArray(8,minNum,maxNum);
+PrintArr(res);
+
+int[] FillArray(int n, int min, int max)
+{
+
+    int[] array = new int[n];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(min, max);
+    }
+    return array;
+
+}
+
+void PrintArr(int[] array)
+{
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i == 0) Console.Write("[");
+
+        if (i < array.Length - 1) Console.Write(array[i] + ", ");
+
+        else Console.Write(array[i] + "] ");
+
+
+    }
+
+}
